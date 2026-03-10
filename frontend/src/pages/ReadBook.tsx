531e32b79df4cc0d17472ceaee1d9ec55fb941ae
Mahmoud Elbana <mahemoudgamal6@gmail.com>
2026-03-09 23:46:32 -0700

    deploy backend to vercel

--- frontend/src/pages/ReadBook.tsx
+++ frontend/src/pages/ReadBook.tsx
@@ -52,7 +52,7 @@ export default function ReadBook() {
     );
   }
 
-  const pdfUrl = `http://localhost:5000${book.fileUrl}`;
+  const pdfUrl = `https://nususi-i5st.vercel.app/register${book.fileUrl}`;
 
   return (
     <div className="min-h-screen bg-gray-900">
